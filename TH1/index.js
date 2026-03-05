3754472074ca351cdfcb518d274265ea7d4591c5
Minh Tam <tamht25072005@gmail.com>
2026-03-05 21:11:01 +0700

    Initial commit - Setup project structure

---
+++ TH1/index.js
@@ -0,0 +1,17 @@
+const http = require('http') // Tạo import http
+const server = http.createServer((req, res) => { //(tạo web sever gồm req và res)
+    console.log(req.url)
+    const url = req.url
+    res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+   if (url === '/' || url === ''){
+    res.end('Trang chủ')
+   } else if (url === '/about'){
+    res.end('Trang giới thiệu')
+   } else if (url === '/contact'){
+    res.end('Trang liên hệ')
+   } else {
+    res.writeHead(404, {'Content-Type': 'text/plain; charset=utf-8'});
+    res.end('Không tìm thấy trang')
+   }
+})
+server.listen(3000)
