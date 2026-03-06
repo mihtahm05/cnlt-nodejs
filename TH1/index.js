4e66ad9bda4453d2ed2cc4833459bc1494f73798
Minh Tam <tamht25072005@gmail.com>
2026-03-06 08:06:18 +0700

    Update index.js

--- TH1/index.js
+++ TH1/index.js
@@ -2,16 +2,17 @@ const http = require('http') // Tạo import http
 const server = http.createServer((req, res) => { //(tạo web sever gồm req và res)
     console.log(req.url)
     const url = req.url
-    res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-   if (url === '/' || url === ''){
-    res.end('Trang chủ')
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    
+   if (url === '/'){
+    res.end('Trang chủ');
    } else if (url === '/about'){
-    res.end('Trang giới thiệu')
+    res.end('Trang giới thiệu');
    } else if (url === '/contact'){
-    res.end('Trang liên hệ')
+    res.end('Trang liên hệ');
    } else {
-    res.writeHead(404, {'Content-Type': 'text/plain; charset=utf-8'});
-    res.end('Không tìm thấy trang')
+    res.statusCode = 404;
+    res.end('Không tìm thấy trang');
    }
 })
 
