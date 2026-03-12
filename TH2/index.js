52a23ccd3c9b503696403c6e8a664ca881aae60b
Minh Tam <tamht25072005@gmail.com>
2026-03-12 21:00:17 +0700

    Upload Folder TH2

---
+++ TH2/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = new express()
+const path = require('path')
+// Đăng kí thư mục public
+app.use(express.static('public'))
+
+app.get('/', (request, response) =>{
+    response.sendFile(path.resolve(__dirname, 'pages/index.html'))
+})
+app.get('/about', (request, response) =>{
+    response.sendFile(path.resolve(__dirname, 'pages/about.html'))
+})
+
+app.get('/contact', (request, response) =>{
+    response.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+})
+app.get('/post', (request, response) =>{
+    response.sendFile(path.resolve(__dirname, 'pages/post.html'))
+})
+app.listen(4000, () => {
+    console.log('App listening on port 4000')
+})
